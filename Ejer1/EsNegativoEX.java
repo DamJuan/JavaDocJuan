bad51faf540fce80eecf642d26890ba7057b874c
Juan <jualopfan@alu.edu.gva.es>
2024-01-24 09:29:33 +0100

    asdasdas

---
+++ Ejer1/EsNegativoEX.java
@@ -0,0 +1,29 @@
+package Ejer1;
+
+/**
+ * La clase {@code EsNegativoEX} es una excepción personalizada para indicar que un valor no puede ser negativo.
+ * Se utiliza en situaciones donde se espera un valor no negativo, pero se proporciona uno negativo.
+ *
+ * <p>Puede construirse con un mensaje de error predeterminado o personalizado.</p>
+ *
+ * @author Tu Nombre
+ * @version 1.0
+ * @since 2024-01-24
+ */
+public class EsNegativoEX extends Exception {
+    /**
+     * Construye un nuevo {@code EsNegativoEX} con un mensaje de error predeterminado.
+     */
+    public EsNegativoEX() {
+        super("El valor no puede ser negativo");
+    }
+
+    /**
+     * Construye un nuevo {@code EsNegativoEX} con un mensaje de error personalizado.
+     *
+     * @param msg Mensaje de error personalizado.
+     */
+    public EsNegativoEX(String msg) {
+        super(msg);
+    }
+}

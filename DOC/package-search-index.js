0a36daa5ec4a3c273d47a8ca64f512475d09d77f
Juan <jualopfan@alu.edu.gva.es>
2024-01-24 09:07:18 +0100

    dasdasd

--- DOC/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Ejer1"}];updateSearchResults();
--- DOC/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Ejer1","l":"EsNegatiuEX"},{"p":"Ejer1","l":"Principal"},{"p":"Ejer1","l":"SocUtil"}];updateSearchResults();
